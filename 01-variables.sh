45f944771f34aa64484bf05a1f5b125d376acaab
Prajwal <prajwalk007.pk@gmail.com>
2026-09-11 12:22:41 +0530

    2nd commit

---
+++ 01-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=Ramesh
+person2=Suresh
+
+echo "$person1 and $person2 are friends"
+
+echo "$person1: hello $person2, how are you?"
+
+echo "$person2: I am fine $person1, how are you?"
---
+++ 03-condition.sh
@@ -0,0 +1,15 @@
+#  if (expersion) {
+#       expersion=true, then statements here will be executed
+#     }
+#  else{
+#       expersion=false, the statemnts here will be executed  
+#      }
+# fi
+
+#!/bin/bash
+if [$number -gt 10]
+then
+    echo "The number is greater than 10"
+else
+    echo "The number is less than or equal to 10"
+    fi
