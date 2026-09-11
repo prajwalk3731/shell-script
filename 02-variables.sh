3479a66047f50604367f0b8fe22d34ca2c17038f
Prajwal <prajwalk007.pk@gmail.com>
2026-09-11 12:03:48 +0530

    add shell variables example

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+app_name=myapp
+environment=production
+version=1.5
+
+echo "Application : $app_name"
+echo "Environment : $environment"
+echo "Version     : $version"
+
+echo "Deploying $app_name version $version to $environment..."
